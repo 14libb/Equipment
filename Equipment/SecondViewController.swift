7f6e6f63cadc34845ce6dcb3dbfd27bf6d17a838
14libb <14libb@gmail.com>
2016-01-17 00:57:25 -0500

    Fixed Delete
    Delete only appears for last table column instead of all possible

--- Equipment/SecondViewController.swift
+++ Equipment/SecondViewController.swift
@@ -58,30 +58,37 @@ class SecondViewController: UIViewController, UITableViewDelegate {
             return cell
         }
     }
+
+    func tableView(tableView: UITableView, editingStyleForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCellEditingStyle {
+        // Detemine if it's in editing mode
+        if (tableView == dateListTable) {
+            return .Delete
+        }
+        return .None
+    }
     
     func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        
-        if editingStyle == UITableViewCellEditingStyle.Delete {
-            if equipmentList[indexPath.row] == "ThinkPad Laptop" {
-                thinkpad.append("ThinkPad Laptop")
+            if editingStyle == UITableViewCellEditingStyle.Delete{
+                if equipmentList[indexPath.row] == "ThinkPad Laptop" {
+                    thinkpad.append("ThinkPad Laptop")
+                }
+                else if equipmentList[indexPath.row] == "SparkFun Kit" {
+                    sparkFun.append("SparkFun Kit")
+                }
+                
+                nameList.removeAtIndex(indexPath.row)
+                equipmentList.removeAtIndex(indexPath.row)
+                dateList.removeAtIndex(indexPath.row)
+                
+                NSUserDefaults.standardUserDefaults().setObject(nameList, forKey: "nameList")
+                NSUserDefaults.standardUserDefaults().setObject(equipmentList, forKey: "equipmentList")
+                NSUserDefaults.standardUserDefaults().setObject(dateList, forKey: "dateList")
+                
+                nameListTable.reloadData()
+                dateListTable.reloadData()
+                equipmentListTable.reloadData()
             }
-            else if equipmentList[indexPath.row] == "SparkFun Kit" {
-                sparkFun.append("SparkFun Kit")
-            }
-            
-            nameList.removeAtIndex(indexPath.row)
-            equipmentList.removeAtIndex(indexPath.row)
-            dateList.removeAtIndex(indexPath.row)
-            
-            NSUserDefaults.standardUserDefaults().setObject(nameList, forKey: "nameList")
-            NSUserDefaults.standardUserDefaults().setObject(equipmentList, forKey: "equipmentList")
-            NSUserDefaults.standardUserDefaults().setObject(dateList, forKey: "dateList")
-            
-            nameListTable.reloadData()
-            dateListTable.reloadData()
-            equipmentListTable.reloadData()
         }
-    }
     
     override func viewDidAppear(animated: Bool) {
         nameListTable.reloadData()
